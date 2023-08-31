a06666719e48412beacfafb1178ef02d5a2ab438
felpscirne <37673574+felpscirne@users.noreply.github.com>
2023-08-30 21:23:29 -0300

    Add files via upload
    esse é bacana

---
+++ LOGICA-2023-2/Triangulo.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Triangulo {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);        
+        int lado1, lado2, lado3, hipotenusa, menorA, menorB;
+        String triangulo;
+        System.out.println("DIGITE OS 3 LADOS DE UM TRIANGULO");
+        lado1 = scan.nextInt();
+        lado2 = scan.nextInt();
+        lado3 = scan.nextInt();
+
+        if ((lado1 == lado2) && (lado1 ==lado3))
+                System.out.println("EQUILATERO");
+        
+        else{ 
+            if ((lado1 > lado2) && (lado1 > lado3)){
+            hipotenusa = lado1;
+            menorA = lado2;
+            menorB = lado3;
+            }else if ((lado2 > lado1) && (lado2 > lado3)){
+                hipotenusa = lado2;
+                menorA = lado1;
+                menorB = lado2;
+            }else{
+                hipotenusa = lado3;
+                menorA = lado1;
+                menorB = lado2;
+        }
+        
+        if (hipotenusa < menorA + menorB) {
+        
+            if ((hipotenusa == menorA) || (menorA == menorB) || (hipotenusa == menorB)){
+                System.out.println("ISÓSCELES");
+                triangulo = "ISOSCELES";
+            }else{
+            System.out.println("ESCALENO");
+            triangulo = "ESCALENO";
+        
+            if (triangulo == "ISOSCELES" || triangulo == "ESCALENO"){  
+                if (hipotenusa * hipotenusa == menorA * menorA + menorB *menorB){
+                    System.out.println("RETANGULO");
+                }    
+            }else{
+                System.out.println("NÃO É UM TRIANGULO VALIDO");
+        }
+    }
+}
+        }
+    }
+}
+
+
